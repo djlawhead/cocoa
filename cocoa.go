faaa77cef2b956de1f004adf4a828c83a877017e
Daniel Lawhead <dlawhead@bitcentral.net>
2017-01-17 13:38:20 -0800

    Attempting to weakly link to Cocoa framework to avoid executable requiring specific version of Mac OS

--- cocoa.go
+++ cocoa.go
@@ -2,8 +2,8 @@ package cocoa
 //+build darwin
 
 /*
-#cgo darwin CFLAGS: -DDARWIN -x objective-c -fobjc-arc -mmacosx-version-min=10.4
-#cgo darwin LDFLAGS: -framework Cocoa
+#cgo darwin CFLAGS: -DDARWIN -x objective-c -fobjc-arc -mmacosx-version-min=10.9
+#cgo darwin LDFLAGS: -weak_framework Cocoa
 
 #include "cocoa.h"
 */
